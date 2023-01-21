a7a04f91eaa699d1227816425f808375a2e84f38
Erik Ström <Erik.Strom@miun.se>
2023-01-21 21:28:52 +0100

    initial commit

---
+++ src/main/java/com/dt199g/laboration1/Solution.java
@@ -0,0 +1,27 @@
+package com.dt199g.laboration1;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+    private void imperativeSolution() { }
+
+    private void declarativeSolution() { }
+
+    /**
+     * Main point of program entry.
+     * MAY NOT BE MODIFIED!
+     * @param args application arguments
+     */
+    public static void main(String... args) {
+        Solution sol = new Solution();
+        String line = "---------------------";
+
+        System.out.printf("%s %n%s %n%s%n", line, "IMPERATIVE SOLUTION", line);
+        sol.imperativeSolution();
+
+        System.out.printf("%n%s %n%s %n%s%n", line, "DECLARATIVE SOLUTION", line);
+        sol.declarativeSolution();
+    }
+}
